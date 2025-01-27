bf16935fa412f14f8e6f7153ff62be45e95f6b47
György Orosz <gyorgy@orosz.link>
2025-01-27 16:23:16 +0100

    feat: cli stub (part II.)
    Using a large context embedder

--- chunking_evaluation/cli.py
+++ chunking_evaluation/cli.py
@@ -1,5 +1,7 @@
 from pprint import pprint
+from typing import cast
 
+from chromadb import Documents, Embeddings
 from chromadb.utils.embedding_functions.sentence_transformer_embedding_function import (
     SentenceTransformerEmbeddingFunction,
 )
@@ -11,11 +13,28 @@ from chunking_evaluation.chunking import RecursiveTokenChunker
 app = Typer()
 
 
+class NomicSentenceTransformerEmbeddingFunction(SentenceTransformerEmbeddingFunction):
+    def __call__(self, input: Documents) -> Embeddings:
+        return cast(
+            Embeddings,
+            [  # noqa: C416
+                embedding
+                for embedding in self._model.encode(
+                    [f"clustering: {d}" for d in input],
+                    convert_to_numpy=True,
+                    normalize_embeddings=self._normalize_embeddings,
+                )
+            ],
+        )
+
+
 @app.command()
 def evaluate():
     chunker = RecursiveTokenChunker(chunk_size=400, chunk_overlap=200)
     evaluation = GeneralEvaluation()
-    embedding = SentenceTransformerEmbeddingFunction(model_name="BAAI/bge-small-en-v1.5", device="cuda")
+    embedding = NomicSentenceTransformerEmbeddingFunction(
+        model_name="nomic-ai/nomic-embed-text-v1.5", device="cuda", trust_remote_code=True
+    )
 
     results = evaluation.run(chunker, embedding)
     pprint(results)
